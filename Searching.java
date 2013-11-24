094d4ae0420ee51f837690e2456bcf24ed6cf640
ChesleyTan <ChesleyTan97@gmail.com>
2013-11-23 19:07:32 -0500

    Implemented methods; replaced individual linSearch methods with unified linSearch() for Comparable args

--- Searching.java
+++ Searching.java
@@ -11,13 +11,13 @@ public class Searching {
       ==================================================*/
     public static int frequency( int[] a, int target ) {
 
-	int freq = 0;
+		int freq = 0;
 
-	for( int i : a ) 
-	    if ( i == target )
-		freq++;
+		for( int i : a ) 
+		    if ( i == target )
+			freq++;
 
-	return freq;
+		return freq;
     }
 
 
@@ -26,22 +26,24 @@ public class Searching {
       post: returns index of first occurrence of target, or
             returns -1 if target not found
       ==================================================*/
-    public static int linSearch( int[] a, int target ) {
+    /*==================================================
+	public static int linSearch( int[] a, int target ) {
 
-	int tPos = -1;
-	int i = 0;
+		int tPos = -1;
+		int i = 0;
 
-	while ( i < a.length ) {
+		while ( i < a.length ) {
 
-	    if ( a[i] == target ) {
-		tPos = i;
-		break;
-	    }
-	    i++;
-	}
+		    if ( a[i] == target ) {
+			tPos = i;
+			break;
+		    }
+		    i++;
+		}
 
-	return tPos;
+		return tPos;
     }
+      ==================================================*/
 
 
     /*==================================================
@@ -49,22 +51,24 @@ public class Searching {
       post: returns index of first occurrence of target, or
             returns -1 if target not found
       ==================================================*/
+    /*==================================================
     public static int linSearch ( String[] a, String target ) {
 
-	int tPos = -1;
-	int i = 0;
+		int tPos = -1;
+		int i = 0;
 
-	while ( i < a.length ) {
+		while ( i < a.length ) {
 
-	    if ( a[i].equals(target) ) {
-		tPos = i;
-		break;
-	    }
-	    i++;
-	}
+		    if ( a[i].equals(target) ) {
+			tPos = i;
+			break;
+		    }
+		    i++;
+		}
 
-	return tPos;
+		return tPos;
     }
+      ==================================================*/
 
 
     /*==================================================
@@ -72,21 +76,23 @@ public class Searching {
       post: returns index of first occurrence of target, or
             returns -1 if target not found
       ==================================================*/
+    /*==================================================
     public static int linSearch ( Binary[] a, Binary target ) {
-	int tPos = -1;
-	int i = 0;
+		int tPos = -1;
+		int i = 0;
 
-	while ( i < a.length ) {
+		while ( i < a.length ) {
 
-	    if ( a[i] == target ) {
-		tPos = i;
-		break;
-	    }
-	    i++;
-	}
+		    if ( a[i] == target ) {
+			tPos = i;
+			break;
+		    }
+		    i++;
+		}
 
-	return tPos;
+		return tPos;
     }
+      ==================================================*/
 
 
     /*==================================================
@@ -95,21 +101,23 @@ public class Searching {
       post: returns index of first occurrence of target, or
             returns -1 if target not found
       ==================================================*/
+    /*==================================================
     public static int linSearch ( Hexadecimal[] a, Hexadecimal target ) {
-	int tPos = -1;
-	int i = 0;
+		int tPos = -1;
+		int i = 0;
 
-	while ( i < a.length ) {
+		while ( i < a.length ) {
 
-	    if ( a[i] == target ) {
-		tPos = i;
-		break;
-	    }
-	    i++;
-	}
+		    if ( a[i] == target ) {
+			tPos = i;
+			break;
+		    }
+		    i++;
+		}
 
-	return tPos;
+		return tPos;
     }
+      ==================================================*/
 
 
     /*==================================================
@@ -120,64 +128,51 @@ public class Searching {
       ==================================================*/
 	//YOUR FUNCTION HERE
     public static int linSearch ( Comparable[] a, Comparable target ) {
-	int tPos = -1;
-	int i = 0;
-
-	while ( i < a.length ) {
-
-	    if ( a[i].equals( target ) ) {
-		tPos = i;
-		break;
-	    }
-	    i++;
-	}
-
-	return tPos;
-	//	return !(a instanceof int || a instanceof String || a instanceof Binary || a instanceof Hexadecimal) ? -1 
-	//	: a instanceof int ? linSearch (((int) a).linSearch((int) target))
-	//	: a instanceof String ? linSearch (((String) a).linSearch((String) target))
-	//	: a instanceof Binary ? linSearch (((Binary) a).linSearch((Binary) target))
-	//	: a instanceof Hexadecimal ? linSearch (((Hexadecimal) a).linSearch((Hexadecimal) target))
-	//	: -1;
-	
+		int tPos = -1;
+		int i = 0;
+		while ( i < a.length ) {
+			if ( a[i].equals( target ) ) {
+				tPos = i;
+				break;
+			}
+		i++;
+		}
+		return tPos;
 	}
 
 
 
     // utility/helper fxn to display contents of an array of Objects
     private static void printArray( int[] arr ) {
-	String output = "[ "; 
+		String output = "[ "; 
 
-	for( int i : arr )
-	    output += i + ", ";
+		for( int i : arr )
+		    output += i + ", ";
 
-	output = output.substring( 0, output.length()-2 ) + " ]";
+		output = output.substring( 0, output.length()-2 ) + " ]";
 
-	System.out.println( output );
-    }
-    private static void printArray( Object[] arr ) {
-	String output = "[ "; 
+		System.out.println( output );
+    	}
+    	private static void printArray( Object[] arr ) {
+		String output = "[ "; 
 
-	for( Object o : arr )
-	    output += o + ", ";
+		for( Object o : arr )
+		    output += o + ", ";
 
-	output = output.substring( 0, output.length()-2 ) + " ]";
+		output = output.substring( 0, output.length()-2 ) + " ]";
 
-	System.out.println( output );
+		System.out.println( output );
     }
 
 
     //main method for testing
     public static void main ( String[] args ) {
 
-
-
-
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	System.out.println("\nNow testing linSearch on int array...");
 
 	//declare and initialize an array of ints
-	int[] iArr = { 2, 4, 6, 8, 6, 42 };
+	Comparable[] iArr = { 2, 4, 6, 8, 6, 42 };
 
 	printArray( iArr );
 
@@ -195,7 +190,7 @@ public class Searching {
 	System.out.println("\nNow testing linSearch on String array...");
 
 	//declare and initialize an array of Strings
-	String[] y = { "kiwi", "watermelon", "orange", "apple", 
+	Comparable[] y = { "kiwi", "watermelon", "orange", "apple", 
 		       "peach", "watermelon" };
 
 	printArray( y );
@@ -214,12 +209,13 @@ public class Searching {
 	System.out.println("\nNow testing linSearch on Binary array...");
 
 	//declare Binary array
-	Binary[] bArr = new Binary[10];
+	Comparable[] bArr = new Binary[10];
 
 	printArray( bArr ); //should show array of nulls
 
 	//initialize Binary array
 	//Q: Why would a FOREACH loop not do the job here?
+	//A FOREACH loop would throw an error when used on a null value
 	for( int i = 0; i < bArr.length; i++ ) {
 	    bArr[i] = new Binary( (int)(100 * Math.random()) );
 	}
@@ -243,12 +239,13 @@ public class Searching {
 	System.out.println("\nNow testing linSearch on Hexadecimal array...");
 
 	//declare Hexadecimal array
-	Hexadecimal[] hArr = new Hexadecimal[10];
+	Comparable[] hArr = new Hexadecimal[10];
 
 	printArray( hArr ); //should show array of nulls
 
 	//initialize Hexadecimal array
 	//Q: Why would a FOREACH loop not do the job here?
+	//A FOREACH loop would throw an error when used on a null value
 	for( int i = 0; i < hArr.length; i++ ) {
 	    hArr[i] = new Hexadecimal( (int)(100 * Math.random()) );
 	}
@@ -269,9 +266,27 @@ public class Searching {
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	System.out.println("\nNow testing linSearch on Comparable array...");
-	//INSERT YOUR TESTING MACHINERY HERE
-	//  Follow the pattern of the machinery above.
+	Comparable[] cArr = new Comparable[10];
+	cArr[0] = (int) (5 * Math.random());
+	cArr[1] = (int) (1000 * Math.random());
+	cArr[2] = "Time for the rationals, the binaries";
+	cArr[3] = "and the hexadecimals";
+	cArr[4] = new Rational((int) (5 * Math.random()), (int) (3 * Math.random()) + 1);
+	cArr[5] = new Rational((int) (5 * Math.random()), (int) (3 * Math.random()) + 1);
+	cArr[6] = new Binary((int) (5 * Math.random() + 1));
+	cArr[7] = new Binary((int) (1000 * Math.random() + 1));
+	cArr[8] = new Hexadecimal((int) (5 * Math.random() + 1));
+	cArr[9] = new Hexadecimal((int) (1000 * Math.random() + 1));
+
+	printArray( cArr ); //should now show array containing ints, Strings, Binaries, and Hexadecimals
+
+	System.out.println( "Searching for " + cArr[3] + "..." );
+	System.out.println( linSearch(cArr, cArr[3] ) );
+
+	System.out.println( "Searching for " + cArr[9] + "..." );
+	System.out.println( linSearch(cArr, cArr[9] ) );
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
     }//end main()
 
